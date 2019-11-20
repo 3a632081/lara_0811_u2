d764234a2cdb42d8d2f53793c066349b9818d9d3
3a632081 <s3a632081@student.ncut.edu.tw>
2019-11-20 18:48:01 +0800

    練習 4：為 Route 命名

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('hello/{name?}',function($name='Everybody'){
-    return'Hello,'.$name;
-});
+Route::get('say/{name?}', ['as'=> 'hello.index',function ($name='everybody') {
+    return 'hello ,'.$name ;
+}]);
 
 Route::get ('/', function(){
     return view('welcome');
